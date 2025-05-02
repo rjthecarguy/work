3b8bd97c3352f39bca6b731640e4a9d3557f994d
RJ <rj@rjrobinson.net>
2025-05-01 22:45:20 -0700

    nav links

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,10 @@
+@props(['url'=>'/','active'=>false, 'icon' => null])
+
+<a href="{{$url}}" class="text-white hover:underline py-2 {{$active ? 'text-yellow-500 font-bold' : ''}}">
+    
+    @if($icon)
+        <i class="fa fa-{{$icon}}"></i>
+    @endif
+    
+    {{$slot}}
+</a>
