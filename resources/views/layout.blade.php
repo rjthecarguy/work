5d82a71e498387132d5d14ceda12d9261a279807
RJ <rj@rjrobinson.net>
2025-05-02 21:26:41 -0700

    menu toggle

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+
 
     <title>{{$title ?? 'Workopia | Find and List Jobs'}}</title>
 </head>
@@ -20,6 +20,6 @@
        {{$slot}}
 
     </main>
-
+<script src="{{asset("js/script.js")}}"></script>
 </body>
 </html>
