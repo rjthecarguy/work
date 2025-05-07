11f74bbde44fa4f7a525264963c0362e1849bd34
RJ <rj@rjrobinson.net>
2025-05-06 17:13:06 -0700

    fixed factories

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -22,22 +22,25 @@ class JobFactory extends Factory
             'title'=>$this->faker->jobTitle(),
             'description'=>$this->faker->paragraphs(2,true),
             'salary'=>$this->faker->numberBetween(40000,120000),
-            'salary'=>implode(', ',$this->faker->words(3)),
+            'tags'=>implode(', ',$this->faker->words(3)),
             'job_type'=>$this->faker->randomElement(['Full-time', 'Part-time', 'Contract']),
             'remote'=>$this->faker->boolean(),
-            'requirements'=>$this->faker->sentances(2,true),
-            'benefits'=>$this->faker->sentances(2,true),
+            'requirements'=>$this->faker->sentences(2,true),
+            'benefits'=>$this->faker->sentences(2,true),
             'address'=>$this->faker->streetAddress(),
             'city'=>$this->faker->city(),
             'state'=>$this->faker->state(),
-            'zipcode'=>$this->faker->poastcode(),
+            'zipcode'=>$this->faker->postcode(),
             'contact_email'=>$this->faker->safeEmail(),
             'contact_phone'=>$this->faker->phoneNumber(),
             'company_name'=>$this->faker->company(),
             'company_description'=>$this->faker->paragraphs(2,true),
             'company_logo'=>$this->faker->imageUrl(100,100,'business',true, 'logo'),
             'company_website'=>$this->faker->url()
+          
+          
 
+            
 
         ];
     }
--- database/migrations/2025_05_06_234002_create_job__listing_table.php
+++ database/migrations/2025_05_06_234002_create_job__listing_table.php
@@ -12,30 +12,38 @@ return new class extends Migration
     public function up(): void
     {
 
-        DB::table('job_listings')->truncate();
+        Schema::dropIfExists('job_listings');
 
-        Schema::table('job_listings', function (Blueprint $table) {
-            
+        Schema::create('job_listings', function (Blueprint $table) {
+
+
             $table->unsignedBigInteger('user_id')->after('id');
-
+            $table->id();
+            $table->string('title');
+            $table->text('description');
             $table->integer('salary');
             $table->string('tags')->nullable();
             $table->enum('job_type',['Full-time', 'Part-time', 'Contract', 'Temporary', 'Volunteer',
-        'Internship', 'On-call'])->default('Full-time');
+            'Internship', 'On-call'])->default('Full-time');
             $table->boolean('remote')->default(false);
             $table->text('requirements')->nullable();
             $table->string('benefits')->nullable();
+            $table->string('address');
             $table->string('city');
             $table->string('state');
             $table->string('zipcode')->nullable();
             $table->string('contact_email');
             $table->string('contact_phone')->nullable();
             $table->string('company_name');
-            $table->string('company_description')->nullable();
+            $table->text('company_description')->nullable();
             $table->string('company_logo')->nullable();
             $table->string('company_website')->nullable();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+
+            $table->timestamps();
+
         });
     }
 
@@ -44,14 +52,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('job_listings', function (Blueprint $table) {
-
-            $table->dropForeign(['user_id']);
-            $table->dropColumn('user_id');
-
-            $table->dropColumn(['salary','tags','job_type','remote','requirements',
-            'benefits','city','state','zipcode','contact_email','contact_phone',
-            'company_name','company_description', 'company_logo','company_website']);
-        });
+        Schema::dropIfExists('job_listings');
     }
 };
