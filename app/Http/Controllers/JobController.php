1529a9405719e5c2fb935aef7b606484ec4ee01d
RJ <rj@rjrobinson.net>
2025-05-07 21:09:20 -0700

    added create page

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -38,10 +38,10 @@ class JobController extends Controller
             'description'=> 'required|string'
         ]);
 
-        Job::create([
-            'title' => $validatedData['title'],
-            'description' => $validatedData['description']
-        ]);
+      //  Job::create([
+      //      'title' => $validatedData['title'],
+      //      'description' => $validatedData['description']
+      //  ]);
 
         return redirect()->route('jobs.index');
     }
