0315a8eed73b2ba6b7dcea19ea4a12ed0cdf01e5
RJ <rj@rjrobinson.net>
2025-05-15 10:26:39 -0700

    added register form

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+class RegisterController extends Controller
+{
+    public function register():View {
+
+        return view('auth.register');
+    }
+}
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,43 @@
+<x-layout>
+
+    <div class="bg-white rounded-lg shadow-md w-full md:max-w-xl mx-auto mt-12 p-8 py-12">
+
+        <h2 class="text-4xl text-center font-bold mb-4">Register</h2>
+
+        <form method="POST" action="{{route('register.store')}}">
+            @csrf
+            <x-inputs.text
+                id="name"
+                name="name"
+                placeholder="Full Name"
+            />
+
+            <x-inputs.text
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Email Address"
+            />
+
+            <x-inputs.text
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            />
+
+            <x-inputs.text
+            id="password_confirmation"
+            name="password_confirmation"
+            placeholder="Confirm Password"
+        />
+
+        <button type="submit"
+            class="w-full bg-blue-500 hover:bg-blue-600 focus:outline-none rounded text-white
+            px-4 py-2">Submit</button>
+
+        </form>
+
+    </div>
+
+</x-layout>
