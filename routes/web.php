b700b6b5137a65bad0051933cb8276acef5d51fe
RJ <rj@rjrobinson.net>
2025-05-15 20:20:06 -0700

    added guest middleware

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,15 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::resource('jobs',JobController::class)->middleware('auth')->only(['create','update','edit','destroy']);
 Route::resource('jobs',JobController::class)->except(['create','update','edit','destroy']);
 
-Route::get('/register', [RegisterController::class, 'register'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+Route::middleware('guest')->group(function(){
+    Route::get('/register', [RegisterController::class, 'register'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    
+    Route::get('/login', [LoginController::class, 'login'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+
+
+}); 
+
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
